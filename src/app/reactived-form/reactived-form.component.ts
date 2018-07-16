c3d286e5161dd60778e54cd2a612b7599927f0b2
Le Zhang <ZhangL@duerkopp-adler.com>
2018-07-16 10:22:31 +0200

    Validation of FormControl, FromGroup

--- src/app/reactived-form/reactived-form.component.ts
+++ src/app/reactived-form/reactived-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, FormBuilder, AbstractControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { Logs } from 'selenium-webdriver';
 
 @Component({
@@ -9,6 +9,31 @@ import { Logs } from 'selenium-webdriver';
 })
 export class ReactivedFormComponent implements OnInit {
 
+  // 自定义formControl的校验方法
+  mobileValidator(mobile: FormControl): any {
+    let value = (mobile.value || '') + '';
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+    let valid = myreg.test(value);
+    console.log("mobile是否校验通过: " + valid);
+    
+    return valid ? null : {mobile: true};
+  }
+
+  // 自定义formGroup的校验方法
+  passwordValidator: ValidatorFn = (info: FormGroup): ValidationErrors | null => {
+    
+    // formGroupName 是passwordInfo
+    let password = info.get('passwordInfo').value.password;
+
+    let passwordConfirm = info.get('passwordInfo').value.passwordConfirm;
+
+    let valid:boolean = password === passwordConfirm;
+    
+    console.log('password 是否校验通过:', valid);
+
+    return valid ? null : {password:true};
+  }
+
   // private xxx: FormControl;
 
   private formModel: FormGroup;
@@ -17,17 +42,24 @@ export class ReactivedFormComponent implements OnInit {
   private fb: FormBuilder = new FormBuilder();
 
   constructor() {
+
+    // 内置校验器
+    // Validators.
+
     this.formModel = this.fb.group({
-      nickName: ['xxxx'],
+      // 添加内置校验器
+      nickName: ['xxxx', [Validators.required, Validators.minLength(6)]],
       emails : this.fb.array([
         [''],
       ]),
-      mobile: [''],
+      // formControl 的校验直接加在数组后面就可以
+      mobile: ['', this.mobileValidator],
+      // formGroup 的校验要加入一个key为validator的object中
       passwordInfo : this.fb.group({
         password: [''],
         passwordConfirm: [''],
       }),
-    });
+    }, {validator: this.passwordValidator});
   }
 
   // constructor() {
@@ -51,6 +83,16 @@ export class ReactivedFormComponent implements OnInit {
   }
 
   createUser() {
+
+    // 获得校验结果
+    let nickNameValid:boolean = this.formModel.get('nickName').valid;
+
+    console.log("nickName valid:", nickNameValid);
+
+    let nickNameErrors:any = this.formModel.get('nickName').errors;
+    console.log('nickName的校验信息: ', JSON.stringify(nickNameErrors));
+    
+
     console.log(this.formModel.value);
     
   }
